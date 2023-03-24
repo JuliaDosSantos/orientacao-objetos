a7960b014e8fb596a3e8891faca244ee13356002
JuliaDosSantos <santos.ju1995@gmail.com>
2023-03-23 23:37:20 -0300

    finalização exercicio 3

--- vetores-exercicio3/src/application/Program.java
+++ vetores-exercicio3/src/application/Program.java
@@ -10,41 +10,51 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 		
-		String nome;
-		int idade;
-		double altura;
-		double soma = 0.0;
-		int pessoasMenosDezesseis = 0;
-		
 		System.out.print("Quantas pessoas serão digitadas? ");
-		
 		int n = scanner.nextInt();
-		double[] vect = new double [n];
+		
+		String[] nome = new String[n];
+		int[] idade = new int[n];
+		double[] altura = new double[n];
 		
 		for (int i = 0; i < n; i++) {
 			System.out.println("Dados da " + (i + 1) + " pessoa");
 			scanner.nextLine();
 			System.out.print("Nome: ");
-			nome = scanner.nextLine();
+			nome[i] = scanner.next();
 			System.out.print("Idade: ");
-			idade = scanner.nextInt();
+			idade[i] = scanner.nextInt();
 			System.out.print("Altura: ");
-			altura = scanner.nextDouble();
-			soma += altura;
-			if (idade < 16) {
-				pessoasMenosDezesseis += 1;
-			}
+			altura[i] = scanner.nextDouble();
 		}
-		 
+		
+		double soma = 0.0;
+		for (int i = 0; i < n; i++) {
+			soma += altura[i];
+		}
+		
 		double mediaAltura = soma / n;
-		double porcentagemMenosDezesseis = pessoasMenosDezesseis / n * 100;
 		
 		System.out.printf("Altura média: %.2f%n", mediaAltura);
-		System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", porcentagemMenosDezesseis);
 		
+		int cont = 0;
+		for (int i = 0; i < n; i++) {
+			if (idade[i] < 16) {
+				cont = cont + 1;
+			}
+		}
+			
+		double porcentagemMenosDezesseis = cont * 100.0 / n;
+
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", porcentagemMenosDezesseis);
 		
+		for (int i = 0; i < n; i++) {
+			if (idade[i] < 16) {
+				System.out.println(nome[i]);
+			}
+		}
+			
 		scanner.close();
-
+		
 	}
-
 }
