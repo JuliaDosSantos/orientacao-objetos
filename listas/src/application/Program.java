df4d0503f5bce673c3756463733ad4197bcf95ca
JuliaDosSantos <santos.ju1995@gmail.com>
2023-04-04 23:41:09 -0300

    listas parte 2

--- listas/src/application/Program.java
+++ listas/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
 
@@ -29,6 +30,21 @@ public class Program {
 		for (String x : list) {
 			System.out.println(x);
 		}
+		
+		System.out.println("-----------------------------");
+		System.out.println("Index of Bob: " + list.indexOf("Bob"));
+		System.out.println("Index of Marco: " + list.indexOf("Marco"));
+		
+		System.out.println("-----------------------------");
+		List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+		
+		for (String x : result) {
+			System.out.println(x);
+		}
+		
+		System.out.println("-----------------------------");
+		String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+		System.out.println(name);
 	}
 
 }
